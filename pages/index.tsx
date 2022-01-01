2b6016f65833ff4a9365f9ed9de96ddfe2270518
samroodAli <devsamrood@gmail.com>
2022-01-01 17:23:07 +0530

    Projects:List all projects using github graphql api and tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,19 @@ import client from "../lib/github-apollo";
 const GET_PROJECTS = gql`
   query {
     viewer {
-      login
+      repositories(first: 100, orderBy: { field: PUSHED_AT, direction: DESC }) {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
     }
   }
 `;
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ projects }: any) => {
   return (
     <>
       <Head>
@@ -22,9 +29,32 @@ const Home: NextPage = () => {
         <title>Samrood Ali</title>
       </Head>
 
-      <main className="bg-red-400 h-screen">My portfolio</main>
+      <main className="bg-red-400">
+        <h1>My portfolio</h1>
+        <p>Projects fetched from github</p>
+        {projects.map(({ node: project }: any) => (
+          <a
+            key={project.id}
+            href={`https://github.com/SamroodAli/${project.name}`}
+          >
+            <div className="p-4 bg-red-200 m-3">{project.name}</div>
+          </a>
+        ))}
+      </main>
     </>
   );
 };
+
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query: GET_PROJECTS,
+  });
+
+  return {
+    props: {
+      projects: data.viewer.repositories.edges,
+    },
+  };
+}
 
 export default Home;
