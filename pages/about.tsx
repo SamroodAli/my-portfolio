58fdaaaab609f7775244bd33f4b749a52f645dd7
samroodAli <devsamrood@gmail.com>
2022-01-01 18:54:12 +0530

    Components:Placeholder about page and clean home page query and opt out serverside props

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,8 +2,9 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { gql } from "@apollo/client";
+import client from "../lib/github-apollo";
 
-const Home: NextPage = ({ projects }: any) => {
+const About: NextPage = ({ projects }: any) => {
   return (
     <>
       <Head>
@@ -14,10 +15,10 @@ const Home: NextPage = ({ projects }: any) => {
       </Head>
 
       <main className="bg-red-400">
-        <h1>Samrood Ali</h1>
+        <h1>Hello, I am Samrood Ali</h1>
       </main>
     </>
   );
 };
 
-export default Home;
+export default About;
