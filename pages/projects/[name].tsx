d75f2e614edef360937198f97a232db45b0bb6b0
samroodAli <devsamrood@gmail.com>
2022-01-02 15:24:53 +0530

    Project:Add foreground backround colors for languages used and language ruby and scss

--- pages/projects/[name].tsx
+++ pages/projects/[name].tsx
@@ -7,13 +7,14 @@ import Link from "next/link";
 const languageColors = {
   TypeScript: { bg: "#2b7489", fg: "#ffffff" },
   JavaScript: { bg: "#f1e05a", fg: "#000" },
-  HTML: { bg: "#e34c26", fg: "#ffffff" },
+  HTML: { bg: "#F02F40", fg: "#ffffff" },
   CSS: { bg: "#563d7c", fg: "#ffffff" },
   Python: { bg: "#3572A5", fg: "#ffffff" },
   Shell: { bg: "#89e051", fg: "#000000" },
   Go: { bg: "#00ADD8", fg: "#ffffff" },
   Dockerfile: { bg: "#384d54", fg: "#ffffff" },
-  Ruby: { bg: "#701516", fg: "#ffffff" },
+  Ruby: { bg: "##CC342D", fg: "#ffffff" },
+  SCSS: { bg: "#C76494", fg: "#ffffff" },
 };
 
 const Project: NextPage = ({ project }: any) => {
