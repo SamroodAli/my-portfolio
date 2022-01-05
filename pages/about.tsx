b2c4a9df24d4b1451d0315374096479998865a01
samroodAli <devsamrood@gmail.com>
2022-01-05 16:38:54 +0530

    About:Data driven about page and populating data

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+const heading = "Hello, I am Samrood Ali";
+const position = "A Full Stack Web Developer obsessed with how things work.";
+const aboutMe = `A Stoic and a Software developer. I wish to apply the principles of
+            stoicism to both life and software development. I fell in love with
+            programming during the corona lockdowns. I love philosophy and read
+            self-help books in my spare time. I am more interested in the science
+            behind or inner engineering of all technologies I come across and
+            haven't limited myself to any "stack". Though I am learning both MERN
+            stack as well Ruby on rails now, I can pick up any stack necessary or
+            fun.
+            `;
+
 const About: NextPage = () => {
   return (
     <>
@@ -12,7 +24,9 @@ const About: NextPage = () => {
       </Head>
 
       <main>
-        <h1>Hello, I am Samrood Ali</h1>
+        <h1>{heading}</h1>
+        <h2>{position}</h2>
+        <p>{aboutMe}</p>
       </main>
     </>
   );
