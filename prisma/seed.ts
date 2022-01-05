9b5180abbea5a4081d0a2ec2db800e5f9274c39e
samroodAli <devsamrood@gmail.com>
2022-01-05 17:05:19 +0530

    Seed:Create query to seed data from github

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,46 @@
-const categories = ["Front-end", "Back-end"];
+import { GraphQLClient, gql } from "graphql-request";
+
+async function main() {
+  const endpoint = "https://api.github.com/graphql";
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    query fetchProjectNames {
+      viewer {
+        id
+        repositories(
+          first: 100
+          privacy: PUBLIC
+          ownerAffiliations: OWNER
+          orderBy: { field: PUSHED_AT, direction: DESC }
+        ) {
+          edges {
+            node {
+              id
+              name
+              description
+              url
+              createdAt
+              updatedAt
+              languages(first: 20) {
+                nodes {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const data = await graphQLClient.request(query);
+  console.log(JSON.stringify(data, undefined, 2));
+}
+
+main().catch((error) => console.error(error));
